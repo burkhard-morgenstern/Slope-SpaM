905a0f5263ffbb94a09ba78e9e0dd577aebdc4d2
Thomas Dencker <thomas.dencker@stud.uni-goettingen.de>
2019-04-01 22:03:34 +0200

    replaced matche array

--- src/Slope_SpaM.cpp
+++ src/Slope_SpaM.cpp
@@ -1,7 +1,6 @@
 /* Slope Spam ist eine Anwendung um die paarweisen Jukes Cantor Distanzen mehrerer (DNA)-Sequenzen zuberechnen und in einer Phylip-Distanzmatrix auszugeben.
  * Es ist eine allignemtfreie Anwendung, welche auf der Anzahl von (spaced) word matchen beruht.*/
 
-// Importe
 #include <iostream>
 #include <string>
 #include <vector>
@@ -9,9 +8,9 @@
 #include <cmath>
 #include <algorithm>
 #include <cstring>
-#include <stdio.h>
+#include <cstdio>
 #include <map>
-#include <math.h>
+#include <cmath>
 #include "args.hpp"
 
 void parse_options(args::ArgumentParser & parser, int argc, char ** argv)
@@ -48,7 +47,7 @@ void read_sequences(std::string & file_name, std::vector<std::string> & sequence
     {
         std::getline( infile, header );
         std::getline( infile, line, '>' );
-		line.erase(std::remove_if(line.begin(), line.end(), [](const char c ){return std::isspace(c);}), line.end());
+		line.erase(std::remove_if(line.begin(), line.end(), [](const char c ){return isspace(c);}), line.end());
 		sequence_names.emplace_back(header);
         sequences.emplace_back(line);
 		lengths.push_back(line.size());
@@ -102,20 +101,17 @@ void create_spaced_words(std::vector<std::vector<int>> & dest, std::string & pat
 	}
 }
 
-std::pair<float, float> calculate_distance(int (*matche)[2], size_t length1, size_t length2, size_t k_range)
+std::pair<float, float> calculate_distance(std::vector<size_t> & matches, size_t length1, size_t length2, size_t kmin)
 {
 	double q = 0.25;
-	std::vector <std::vector<long double>> werte;
-	for (int w=0; w<k_range; w++){
-		if (matche[w][0] != 0){
-			long double x = pow(q, matche[w][0]) * length1 * length2;
-			long double y = log (matche[w][1] -x);
-			std::vector<long double> wertepaar;
+	std::vector <std::pair<long double, long double>> werte;
+	for (int w=0; w<matches.size(); w++){
+		if (matches[w] != 0){
+			long double x = pow(q, kmin + w + 1) * length1 * length2; // TODO:again, why + 1
+			long double y = log (matches[w] - x);
 			if (isnan(y) == false){
-				wertepaar.emplace_back(matche[w][0]);
-				wertepaar.emplace_back(y);
+				werte.emplace_back(kmin + w + 1, y);
 			}
-			werte.emplace_back(wertepaar);
 		}
 	}
 	double m = 0; 
@@ -124,18 +120,18 @@ std::pair<float, float> calculate_distance(int (*matche)[2], size_t length1, siz
 	double x_sum=0;
 	double y_sum=0;
 	for (int i=0; i<werte.size(); i++){
-		x_sum = x_sum + werte[i][0];
+		x_sum += werte[i].first;
 	}
 	for (int i=0; i<werte.size(); i++){
-		y_sum = y_sum + werte[i][1];
+		y_sum += werte[i].second;
 	}	
 	x_mean = double(1.0/werte.size()) * x_sum;
 	y_mean = double(1.0/werte.size()) *y_sum;
 	double Zaehler = 0;
 	double Nenner = 0;
 	for (int i=0; i<werte.size(); i++){
-		Zaehler = Zaehler + (werte[i][0]-x_mean)*(werte[i][1]-y_mean);
-		Nenner = Nenner + (werte[i][0]-x_mean)*(werte[i][0]-x_mean);
+		Zaehler = Zaehler + (werte[i].first-x_mean)*(werte[i].second-y_mean);
+		Nenner = Nenner + (werte[i].first-x_mean)*(werte[i].first-x_mean);
 	}
 	m = Zaehler/Nenner;
 	double p = exp(m);
@@ -156,55 +152,22 @@ std::vector<std::vector<double>> calculate_distance_matrix(std::vector<std::stri
 			std::sort(wordlistges.begin(), wordlistges.end()); 
 			int Lmax = std::max(Lens[i], Lens[j]);
 			int kmin =  log (2 * Lmax ) / 0.87 + 1;
-			std::map <int, int> matchZahl;
-			int matche [kmax-kmin][2]={0};
-			for (int k=kmin; k<kmax; k++){
-				for (int z=1; z<wordlistges.size(); z++){
-					if (z==1){ //erstes paar
-						if (std::equal(wordlistges[z-1].begin(), wordlistges[z-1].begin()+k+1, wordlistges[z].begin())){
-							matchZahl[wordlistges[z-1][wordlistges[z-1].size()-1]]++;
-							matchZahl[wordlistges[z][wordlistges[z].size()-1]]++;
-						}
-					}
-					else { 
-						if (std::equal(wordlistges[z-1].begin(), wordlistges[z-1].begin()+k+1, wordlistges[z].begin())){
-							if (matchZahl.size()>0){//Match mit vorherigen
-								matchZahl[wordlistges[z][wordlistges[z].size()-1]]++;							
-							}
-							else{
-								matchZahl[wordlistges[z-1][wordlistges[z-1].size()-1]]++;
-								matchZahl[wordlistges[z][wordlistges[z].size()-1]]++;
-							}
-						}
-						else{//Mismatch
-							if (matchZahl.size()>1){
-								std::vector<int> v;
-								for(std::map<int, int>::iterator iter = matchZahl.begin(); iter != matchZahl.end(); ++iter){
-									v.push_back( iter->second );
-								}
-								int match = v[0]*v[1];
-								matche [k-kmin][0] = k+1;
-								matche [k-kmin][1] = matche [k-kmin][1] + match;
-							}
-							matchZahl.clear();
-						}
-					}
-					if (z==wordlistges.size()-1){//ggf leztes Match eintragen
-						if (matchZahl.size()>1){
-							std::vector<int> v;
-							for(std::map<int, int>::iterator iter = matchZahl.begin(); iter != matchZahl.end(); ++iter){
-								v.push_back( iter->second );
-							}
-							int match = v[0]*v[1];
-							matche [k-kmin][0] = k+1;
-							matche [k-kmin][1] = matche [k-kmin][1] + match;
-						}
-						matchZahl.clear();
-					}
+			std::vector<size_t> matches(kmax - kmin);
+			for (int k = kmin; k < kmax; k++) // TODO: kmax not used?
+			{
+				for(auto it = wordlistges.begin(); it != wordlistges.end();)
+				{
+					auto next = std::find_if_not(it, wordlistges.end(), [=](const std::vector<int> & word)
+																		{return std::equal(word.begin(), word.begin() + k + 1, it->begin());});
+					int seq_i_count = std::count_if(it, next, [=](std::vector<int> & word){return word.back() == i;});
+					int seq_j_count = std::distance(it, next) - seq_i_count;
+					// matche[k - kmin][0] = k + 1; // TODO: why was there k+1 here?
+					matches[k - kmin] += seq_i_count * seq_j_count;
+					it = next;
 				}
 			} 
 			//y werte berrechnen
-			auto values = calculate_distance(matche, Lens[i], Lens[j], kmax - kmin);
+			auto values = calculate_distance(matches, Lens[i], Lens[j], kmin);
 			std::cout  << "match probability p : " << values.first << " Jukes-Cantor distance d : " << values.second <<'\n' ;
 			distance [i][j] = values.second;
 			distance [j][i] = values.second; 
