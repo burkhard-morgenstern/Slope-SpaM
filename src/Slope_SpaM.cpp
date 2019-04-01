ad1153a3f058dbe5471f2fdd1691a00d2281bfae
Thomas Dencker <thomas.dencker@stud.uni-goettingen.de>
2019-04-01 16:10:42 +0200

    some refactoring

--- src/Slope_SpaM.cpp
+++ src/Slope_SpaM.cpp
@@ -28,6 +28,162 @@ int char_count (std::string pattern){
 	return kmax;
 }
 
+void create_spaced_words(std::vector<std::vector<int>> & dest, std::string & pattern, std::vector<std::string> & Seqs, size_t seq_num, size_t length)
+{
+	for (int iter=1; iter<length-pattern.size()+2; iter++){
+		auto substring = Seqs[seq_num].begin() + iter;
+		std::vector <int> vecword;
+		for (int k=0; k<pattern.size(); k++){
+			if (pattern[k] == '1'){
+				if (substring[k] == 'A'){
+					vecword.push_back(1);
+				}
+				else if (substring[k] == 'C'){
+					vecword.push_back(2);
+				}
+				else if (substring[k] == 'G'){
+					vecword.push_back(3);
+				}
+				else if (substring[k] == 'T'){
+					vecword.push_back(4);
+				}
+				else if (substring[k] == ' '){
+				} 
+				else{
+					std::cout << "Your sequences also contain other characters than 'A', 'C', 'G' and 'T'. Please checḱ your Input Data."<< '\n';
+					std::cout << substring[k] << '\n';
+					exit (EXIT_FAILURE);
+				}					
+			}
+		}
+		vecword.emplace_back(seq_num);
+		dest.emplace_back(vecword);
+	}
+}
+
+std::pair<float, float> calculate_distance(int (*matche)[2], size_t length1, size_t length2, size_t k_range)
+{
+	double q = 0.25;
+	std::vector <std::vector<long double>> werte;
+	for (int w=0; w<k_range; w++){
+		if (matche[w][0] != 0){
+			long double x = pow(q, matche[w][0]) * length1 * length2;
+			long double y = log (matche[w][1] -x);
+			std::vector<long double> wertepaar;
+			if (isnan(y) == false){
+				wertepaar.emplace_back(matche[w][0]);
+				wertepaar.emplace_back(y);
+			}
+			werte.emplace_back(wertepaar);
+		}
+	}
+	double m = 0; 
+	double x_mean =0;
+	double y_mean=0;
+	double x_sum=0;
+	double y_sum=0;
+	for (int i=0; i<werte.size(); i++){
+		x_sum = x_sum + werte[i][0];
+	}
+	for (int i=0; i<werte.size(); i++){
+		y_sum = y_sum + werte[i][1];
+	}	
+	x_mean = double(1.0/werte.size()) * x_sum;
+	y_mean = double(1.0/werte.size()) *y_sum;
+	double Zaehler = 0;
+	double Nenner = 0;
+	for (int i=0; i<werte.size(); i++){
+		Zaehler = Zaehler + (werte[i][0]-x_mean)*(werte[i][1]-y_mean);
+		Nenner = Nenner + (werte[i][0]-x_mean)*(werte[i][0]-x_mean);
+	}
+	m = Zaehler/Nenner;
+	double p = exp(m);
+	double d = -(3.0/4.0) * log(1 - (4.0/3.0) * (1-p));
+	return {p, d};
+}
+
+std::vector<std::vector<double>> calculate_distance_matrix(std::vector<std::string> & Seqs, std::string & pattern, std::vector<size_t> & Lens, size_t kmax)
+{
+	std::vector<std::vector<double>> distance(Seqs.size(), std::vector<double>(Seqs.size(), 0));
+	// double distance [Seqs.size()] [Seqs.size()] = {0.00000};
+	for (int i=0; i<Seqs.size()-1; i++){
+		std::cout<<"sequence number : "<<i<<'\n';
+		std::vector <std::vector<int>> wordlist1;
+		create_spaced_words(wordlist1, pattern, Seqs, i, Lens[i]);
+		for (int j=i+1; j<Seqs.size(); j++){
+			std::vector <std::vector<int>> wordlistges(wordlist1.begin(), wordlist1.end());
+			create_spaced_words(wordlistges, pattern, Seqs, j, Lens[j]);
+			std::sort(wordlistges.begin(), wordlistges.end()); 
+			int Lmax;
+			if (Lens[i]>Lens[j]){
+				Lmax = Lens[i];
+			}
+			else {
+				Lmax=Lens[j];
+			}
+			int kmin =  log (2 * Lmax ) / 0.87 + 1;
+			std::map <int, int> matchZahl;
+			int matche [kmax-kmin][2]={0};
+			for (int k=kmin; k<kmax; k++){
+				for (int z=1; z<wordlistges.size(); z++){
+					if (z==1){ //erstes paar
+						if (std::equal(wordlistges[z-1].begin(), wordlistges[z-1].begin()+k+1, wordlistges[z].begin())){
+							matchZahl[wordlistges[z-1][wordlistges[z-1].size()-1]]++;
+							matchZahl[wordlistges[z][wordlistges[z].size()-1]]++;
+						}
+					}
+					else { 
+						if (std::equal(wordlistges[z-1].begin(), wordlistges[z-1].begin()+k+1, wordlistges[z].begin())){
+							if (matchZahl.size()>0){//Match mit vorherigen
+								matchZahl[wordlistges[z][wordlistges[z].size()-1]]++;							
+							}
+							else{
+								matchZahl[wordlistges[z-1][wordlistges[z-1].size()-1]]++;
+								matchZahl[wordlistges[z][wordlistges[z].size()-1]]++;
+							}
+						}
+						else{//Mismatch
+							if (matchZahl.size()>1){
+								std::vector<int> v;
+								for(std::map<int, int>::iterator iter = matchZahl.begin(); iter != matchZahl.end(); ++iter){
+									v.push_back( iter->second );
+								}
+								int match = v[0]*v[1];
+								matche [k-kmin][0] = k+1;
+								matche [k-kmin][1] = matche [k-kmin][1] + match;
+								matchZahl.clear();	
+							}
+							else{
+								matchZahl.clear();
+							}
+						}
+					}
+					if (z==wordlistges.size()-1){//ggf leztes Match eintragen
+						if (matchZahl.size()>1){
+							std::vector<int> v;
+							for(std::map<int, int>::iterator iter = matchZahl.begin(); iter != matchZahl.end(); ++iter){
+								v.push_back( iter->second );
+							}
+							int match = v[0]*v[1];
+							matche [k-kmin][0] = k+1;
+							matche [k-kmin][1] = matche [k-kmin][1] + match;
+							matchZahl.clear();	
+						}
+						else{
+							matchZahl.clear();
+						}
+					}
+				}
+			} 
+			//y werte berrechnen
+			auto values = calculate_distance(matche, Lens[i], Lens[j], kmax - kmin);
+			std::cout  << "match probability p : " << values.first << " Jukes-Cantor distance d : " << values.second <<'\n' ;
+			distance [i][j] = values.second;
+			distance [j][i] = values.second; 
+		}
+	}
+	return distance;
+}
 
 int main (int argc, char** argv){
 	args::ArgumentParser parser("Slope-SpaM");
@@ -53,6 +209,11 @@ int main (int argc, char** argv){
 
 	//Einlesen der Sequenzen aus der Fasta-Datei in zwei Vektoren
 	std::ifstream file (infile.Get());
+	if(file.is_open() == false)
+	{
+		std::cerr << "File could not be opened: " << infile.Get() << std::endl;
+		return -1;
+	}
 	std::vector <std::string> SeqKeys; // Vektor für die Namen der Sequenzen
 	std::vector <std::string> Seqs; // Vektor für die Sequenzen als ein String
 	std::string SeqK;
@@ -77,8 +238,8 @@ int main (int argc, char** argv){
 	Seqs.emplace_back(Seq);
 	std::cout << "Data imported" << '\n'; // Die Sequenzen und ihre Namen wurden in den entsprechenden Vektoren gespeichert
 	// Berechnen der Längen aller Sequenzen und Speichern in einem Vektor
-	std::vector <int> Lens; // Vektor für die Längen der Sequenzen
-	int len;
+	std::vector <size_t> Lens; // Vektor für die Längen der Sequenzen
+	size_t len;
 	for (int i=0; i<Seqs.size(); i++){
 		len = Seqs[i].length()-1;
 		Lens.emplace_back(len);
@@ -99,196 +260,8 @@ int main (int argc, char** argv){
 	std::ofstream f (outfile.Get());
 	f << SeqZahl << '\n';
 	f.close();
-	double distance [SeqZahl] [SeqZahl] = {0.00000};
-	for (int i=0; i<Seqs.size()-1; i++){
-		std::cout<<"sequence number : "<<i<<'\n';
-		std::vector <std::vector<int>> wordlist1;
-		for (int iter=1; iter<Lens[i]-lenPat+2; iter++){
-			std::string substring = Seqs[i].substr(iter, lenPat);
-			std::string word;
-			std::vector <int> vecword;
-			for (int k=0; k<lenPat; k++){
-				if (pattern[k] == '1'){
-					if (substring[k] == 'A'){
-						word.append("1");
-					}
-					else if (substring[k] == 'C'){
-						word.append("2");
-					}
-					else if (substring[k] == 'G'){
-						word.append("3");
-					}
-					else if (substring[k] == 'T'){
-						word.append("4");
-					}
-					else if (substring[k] == ' '){
-					} 
-					else{
-						std::cout << "Your sequences also contain other characters than 'A', 'C', 'G' and 'T'. Please checḱ your Input Data."<< '\n';
-						std::cout << substring[k] << '\n';
-						exit (EXIT_FAILURE);
-					}					
-				}
-				
-				
-			}
-			for (int l=0; l<word.length(); l+=1){
-				std::string tw = word.substr(l, 1);
-				int zahlword =  std::atoi (tw.c_str());
-				vecword.emplace_back(zahlword);
-			}
-			vecword.emplace_back(i);
-			wordlist1.emplace_back(vecword);
-		}
-		for (int j=i+1; j<Seqs.size(); j++){
-			std::vector <std::vector<int>> wordlist2;
-			for (int iter=1; iter<Lens[j]-lenPat+2; iter++){
-				std::string substring = Seqs[j].substr(iter, lenPat);
-				std::string word;
-				std::vector <int> vecword;
-				for (int k=0; k<lenPat; k++){
-					if (pattern[k] == '1'){
-						if (substring[k] == 'A'){
-							word.append("1");
-						}
-						else if (substring[k] == 'C'){
-							word.append("2");
-						}
-						else if (substring[k] == 'G'){
-							word.append("3");
-						}
-						else if (substring[k] == 'T'){
-							word.append("4");
-						}
-						else if (substring[k] == ' '){
-						} 
-						else{
-							std::cout << "Your sequences also contain other characters than 'A', 'C', 'G' and 'T'. Please checḱ your Input Data."<< '\n';
-							std::cout << substring[k] << '\n';
-							exit (EXIT_FAILURE);
-						}								
-					}
-				
-				}
-				for (int l=0; l<word.length(); l+=1){
-					std::string tw = word.substr(l, 1);
-					int zahlword =  std::atoi (tw.c_str());
-					vecword.emplace_back(zahlword);
-				}
-				vecword.emplace_back(j);
-				wordlist2.emplace_back(vecword);
-			}
-			std::vector <std::vector<int>> wordlistges;
-			wordlistges.insert (wordlistges.end(), wordlist1.begin(), wordlist1.end());
-			wordlistges.insert (wordlistges.end(), wordlist2.begin(), wordlist2.end());
-			std::sort(wordlistges.begin(), wordlistges.end()); 
-			int Lmax;
-			if (Lens[i]>Lens[j]){
-				Lmax = Lens[i];
-			}
-			else {
-				Lmax=Lens[j];
-			}
-			int kmin =  log (2 * Lmax ) / 0.87 + 1;
-			std::map <int, int> matchZahl;
-			int matche [kmax-kmin][2]={0};
-			for (int k=kmin; k<kmax; k++){ 
-				for (int z=1; z<wordlistges.size(); z++){
-					if (z==1){ //erstes paar
-						if (std::equal(wordlistges[z-1].begin(), wordlistges[z-1].begin()+k+1, wordlistges[z].begin())){
-							matchZahl[wordlistges[z-1][wordlistges[z-1].size()-1]] = matchZahl[wordlistges[z-1][wordlistges[z-1].size()-1]]+1;
-							matchZahl[wordlistges[z][wordlistges[z].size()-1]] = matchZahl[wordlistges[z][wordlistges[z].size()-1]]+1;
-						}
-					}
-					else { 
-						if (std::equal(wordlistges[z-1].begin(), wordlistges[z-1].begin()+k+1, wordlistges[z].begin())){
-							if (matchZahl.size()>0){//Match mit vorherigen
-								matchZahl[wordlistges[z][wordlistges[z].size()-1]] = matchZahl[wordlistges[z][wordlistges[z].size()-1]]+1;								
-							}
-							else{
-								matchZahl[wordlistges[z-1][wordlistges[z-1].size()-1]] = matchZahl[wordlistges[z-1][wordlistges[z-1].size()-1]]+1;
-								matchZahl[wordlistges[z][wordlistges[z].size()-1]] = matchZahl[wordlistges[z][wordlistges[z].size()-1]]+1;
-							}
-						}
-						else{//Mismatch
-							if (matchZahl.size()>1){
-								std::vector<int> v;
-								for(std::map<int, int>::iterator iter = matchZahl.begin(); iter != matchZahl.end(); ++iter){
-									v.push_back( iter->second );
-								}	
-								int match = v[0]*v[1];
-								matche [k-kmin][0] = k+1;
-								matche [k-kmin][1] = matche [k-kmin][1] + match;
-								matchZahl.clear();	
-							}
-							else{
-								matchZahl.clear();
-							}
-						}
-					}
-					if (z==wordlistges.size()-1){//ggf leztes Match eintragen
-						if (matchZahl.size()>1){
-							std::vector<int> v;
-							for(std::map<int, int>::iterator iter = matchZahl.begin(); iter != matchZahl.end(); ++iter){
-								v.push_back( iter->second );
-							}	
-							int match = v[0]*v[1];
-							matche [k-kmin][0] = k+1;
-							matche [k-kmin][1] = matche [k-kmin][1] + match;
-							matchZahl.clear();	
-						}
-						else{
-							matchZahl.clear();
-						}
-					}
-				}
-		} 
-		//y werte berrechnen
-		double q = 0.25;
-		std::vector <std::vector<long double>> werte;
-		for (int w=0; w<kmax-kmin; w++){
-			if (matche[w][0] != 0){
-			long double x = pow(q, matche[w][0]) * Lens[i] * Lens[j];
-			long double y = log (matche[w][1] -x);
-			std::vector<long double> wertepaar;
-			if (isnan(y)){
-			}
-			else{
-				wertepaar.emplace_back(matche[w][0]);
-				wertepaar.emplace_back(y);
-			}
-			werte.emplace_back(wertepaar);
-		}
-		}
-		double m = 0; 
-		double x_mean =0;
-		double y_mean=0;
-		double x_sum=0;
-		double y_sum=0;
-		for (int i=0; i<werte.size(); i++){
-			x_sum = x_sum + werte[i][0];
-		}
-		for (int i=0; i<werte.size(); i++){
-			y_sum = y_sum + werte[i][1];
-		}	
-		x_mean = double(1.0/werte.size()) * x_sum;
-		y_mean = double(1.0/werte.size()) *y_sum;
-		double Zaehler;
-		double Nenner;
-		for (int i=0; i<werte.size(); i++){
-			Zaehler = Zaehler + (werte[i][0]-x_mean)*(werte[i][1]-y_mean);
-			Nenner = Nenner + (werte[i][0]-x_mean)*(werte[i][0]-x_mean);
-		}
-		m = Zaehler/Nenner;
-		double p = exp(m);
-		double d = -(3.0/4.0) * log(1 - (4.0/3.0) * (1-p));
-		std::cout  << "match probability p : " << p << " Jukes-Cantor distance d : " << d <<'\n' ;
-		distance [i][j] = d;
-		distance [j][i] = d; 
-		wordlist2.clear();
-		wordlistges.clear();
-}
-}
+	
+	auto distance = calculate_distance_matrix(Seqs, pattern, Lens, kmax);
 	std::ofstream ff (outfile.Get(), std::ios::app);
 	for (int i=0; i<SeqZahl; i++){
 		ff << SeqKeys [i] << '\t';
@@ -298,21 +271,4 @@ int main (int argc, char** argv){
 		ff <<'\n';
 	}
 	ff.close();
-	std::cout << "finished"<<'\n';
 }
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
