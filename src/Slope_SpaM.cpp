1846d6d10ce93bccfc4de203f6f0870deda08e4f
Thomas Dencker <thomas.dencker@stud.uni-goettingen.de>
2019-04-01 17:41:45 +0200

    more refactoring and fixed illegal character handling

--- src/Slope_SpaM.cpp
+++ src/Slope_SpaM.cpp
@@ -14,23 +14,64 @@
 #include <math.h>
 #include "args.hpp"
 
+void parse_options(args::ArgumentParser & parser, int argc, char ** argv)
+{
+	try
+    {
+        parser.ParseCLI(argc, argv);
+    }
+    catch (args::Help)
+    {
+        std::cout << parser;
+        exit(0);
+    }
+    catch (args::Error e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        exit(-1);
+    }
+}
 
-// berechnet kmax, das pattern weight
-int char_count (std::string pattern){
-	int i = 0;
-	int kmax = 0;
+void read_sequences(std::string & file_name, std::vector<std::string> & sequences, std::vector<std::string> & sequence_names, std::vector<size_t> & lengths)
+{
+    std::ifstream infile(file_name);
+    if ( infile.is_open() == false )
+    {
+        std::cerr << "File " + file_name + " could not be opened!";
+		exit(-1);
+    }
+    std::string line;
+    std::string header;
+    std::getline( infile, line, '>' );
+    while ( !infile.eof() )
+    {
+        std::getline( infile, header );
+        std::getline( infile, line, '>' );
+		line.erase(std::remove_if(line.begin(), line.end(), [](const char c ){return std::isspace(c);}), line.end());
+		sequence_names.emplace_back(header);
+        sequences.emplace_back(line);
+		lengths.push_back(line.size());
+	}
+	std::cout << sequences.size() << " sequences have been read." << std::endl;
+}
+
+int check_pattern(std::string & pattern, std::vector<size_t> & Lens)
+{
 	int lenPat = pattern.length();
-	for  (int i = 0; i < lenPat; i++){
-		if (pattern[i] == '1') {
-			kmax++;
-			}
-		}
+	int kmax = std::count_if(pattern.begin(), pattern.end(), [](const char c){return c == '1';});
+	int Lm  = *std::max_element(Lens.begin(), Lens.end());
+	int km =  log (2 * Lm ) / 0.87 + 1;
+	if (kmax-km < 10){
+		std::cout << "Your pattern weight is too small for your sequences, there should be a least 10 value calculable. Please use a pattern with a higher pattern weight."<< '\n';
+		exit(-1);
+	}
 	return kmax;
 }
 
 void create_spaced_words(std::vector<std::vector<int>> & dest, std::string & pattern, std::vector<std::string> & Seqs, size_t seq_num, size_t length)
 {
-	for (int iter=1; iter<length-pattern.size()+2; iter++){
+	for (int iter=0; iter<length-pattern.size()+2; iter++){
 		auto substring = Seqs[seq_num].begin() + iter;
 		std::vector <int> vecword;
 		for (int k=0; k<pattern.size(); k++){
@@ -47,14 +88,14 @@ void create_spaced_words(std::vector<std::vector<int>> & dest, std::string & pat
 				else if (substring[k] == 'T'){
 					vecword.push_back(4);
 				}
-				else if (substring[k] == ' '){
-				} 
 				else{
-					std::cout << "Your sequences also contain other characters than 'A', 'C', 'G' and 'T'. Please checḱ your Input Data."<< '\n';
-					std::cout << substring[k] << '\n';
-					exit (EXIT_FAILURE);
+					break;
 				}					
 			}
+		}
+		if(vecword.size() < pattern.size())
+		{
+			continue; // illegal characters
 		}
 		vecword.emplace_back(seq_num);
 		dest.emplace_back(vecword);
@@ -105,7 +146,6 @@ std::pair<float, float> calculate_distance(int (*matche)[2], size_t length1, siz
 std::vector<std::vector<double>> calculate_distance_matrix(std::vector<std::string> & Seqs, std::string & pattern, std::vector<size_t> & Lens, size_t kmax)
 {
 	std::vector<std::vector<double>> distance(Seqs.size(), std::vector<double>(Seqs.size(), 0));
-	// double distance [Seqs.size()] [Seqs.size()] = {0.00000};
 	for (int i=0; i<Seqs.size()-1; i++){
 		std::cout<<"sequence number : "<<i<<'\n';
 		std::vector <std::vector<int>> wordlist1;
@@ -185,90 +225,36 @@ std::vector<std::vector<double>> calculate_distance_matrix(std::vector<std::stri
 	return distance;
 }
 
+void print_distance_matrix(std::string outfile, size_t num_sequences, std::vector<std::string> & SeqKeys, std::vector<std::vector<double>> & distance)
+{
+	std::ofstream ff (outfile, std::ios::app);
+	ff << num_sequences << std::endl;
+	for (int i=0; i<num_sequences; i++){
+		ff << SeqKeys [i] << '\t';
+		for (int k=0; k<num_sequences; k++){
+			ff << distance[i][k] <<'\t';
+		}
+		ff << std::endl;
+	}
+	ff.close();
+}
+
 int main (int argc, char** argv){
 	args::ArgumentParser parser("Slope-SpaM");
 	args::ValueFlag<std::string> infile(parser, "Input file", "Specify input file name", {'i', "input"}, args::Options::Required);
 	args::ValueFlag<std::string> outfile(parser, "Output file", "Specify output file name", {'o', "output"}, "out.dmat");
 	args::ValueFlag<std::string> pattern_flag(parser, "Pattern", "Use this pattern if specified", {'p', "pattern"}, "111111111111111111111111111111111111");
 	args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
-	try
-    {
-        parser.ParseCLI(argc, argv);
-    }
-    catch (args::Help)
-    {
-        std::cout << parser;
-        return 0;
-    }
-    catch (args::Error e)
-    {
-        std::cerr << e.what() << std::endl;
-        std::cerr << parser;
-        return 1;
-    }
+	parse_options(parser, argc, argv);
 
-	//Einlesen der Sequenzen aus der Fasta-Datei in zwei Vektoren
-	std::ifstream file (infile.Get());
-	if(file.is_open() == false)
-	{
-		std::cerr << "File could not be opened: " << infile.Get() << std::endl;
-		return -1;
-	}
-	std::vector <std::string> SeqKeys; // Vektor für die Namen der Sequenzen
-	std::vector <std::string> Seqs; // Vektor für die Sequenzen als ein String
-	std::string SeqK;
-	std::string SeqKey;
-	std::string Seq;
-	std::string line;
-	while (getline(file, line)){
-		if (line [0] == '>') {
-			if (!Seq.empty()){
-				Seqs.emplace_back(Seq);
-			}
-			SeqK = line;
-			Seq = '\0';
-			SeqKey = SeqK.substr(1);
-			SeqKeys.emplace_back(SeqKey);
-		}
-		else {
-			Seq = Seq + line;
-		}
-	}
-	
-	Seqs.emplace_back(Seq);
-	std::cout << "Data imported" << '\n'; // Die Sequenzen und ihre Namen wurden in den entsprechenden Vektoren gespeichert
-	// Berechnen der Längen aller Sequenzen und Speichern in einem Vektor
-	std::vector <size_t> Lens; // Vektor für die Längen der Sequenzen
-	size_t len;
-	for (int i=0; i<Seqs.size(); i++){
-		len = Seqs[i].length()-1;
-		Lens.emplace_back(len);
-	}
-	// Verarbeiten des Patterns
+	std::vector <std::string> SeqKeys;
+	std::vector <std::string> Seqs;
+	std::vector <size_t> Lens;
+	read_sequences(infile.Get(), Seqs, SeqKeys, Lens);
+
 	std::string pattern = pattern_flag.Get();
-	int lenPat = pattern.length();  // Länge des Pattern
-	int kmax; // pattern weight, Anzahl der Matchpositionen im Pattern
-	kmax = char_count(pattern);
-	int Lm  = *std::max_element(Lens.begin(), Lens.end());
-	int km =  log (2 * Lm ) / 0.87 + 1;
-	if (kmax-km < 10){
-		std::cout << "Your pattern weight is too small for your sequences, there should be a least 10 value calculable. Please use a pattern with a higher pattern weight."<< '\n';
-		exit (EXIT_FAILURE);
-	}
-	int SeqZahl = Lens.size();	// Anzahl der Sequenzen	
-	// Erstellen der Outputdatei und eintragen der Anzahl der Sequenzen
-	std::ofstream f (outfile.Get());
-	f << SeqZahl << '\n';
-	f.close();
-	
+	int kmax = check_pattern(pattern, Lens);
+
 	auto distance = calculate_distance_matrix(Seqs, pattern, Lens, kmax);
-	std::ofstream ff (outfile.Get(), std::ios::app);
-	for (int i=0; i<SeqZahl; i++){
-		ff << SeqKeys [i] << '\t';
-		for (int k=0; k<SeqZahl; k++){
-			ff << distance[i][k] <<'\t';
-		}
-		ff <<'\n';
-	}
-	ff.close();
+	print_distance_matrix(outfile.Get(), Seqs.size(), SeqKeys, distance);
 }
